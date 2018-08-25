e193a6bef3b757195c2ba0887cce656e76c802da
David Szabo <david.szabo97@gmail.com>
2018-08-25 21:41:09 +0200

    Fix #4 mobile compatibility

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,46 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import ReduxTaskList from '../redux/containers/task-list'
 import ReduxSidebarConnected from '../redux/containers/sidebar'
+import ReduxBottomNavigationBarConnected from '../redux/containers/bottom-navigation-bar';
+import { connect } from 'react-redux'
 
-const Main = () => (
+const MainDesktop = () => (
   <main>
     <ReduxSidebarConnected />
     <ReduxTaskList />
   </main>
 )
+
+const MainMobile = ({ bottomNavigationBarSelectedItemIndex }) => (
+  <main>
+    {bottomNavigationBarSelectedItemIndex === 0 ? <ReduxSidebarConnected /> : <ReduxTaskList />}
+    <ReduxBottomNavigationBarConnected />
+  </main>
+)
+
+const mapStateToProps = state => ({
+  bottomNavigationBarSelectedItemIndex: state.bottomNavigationBar.selectedItemIndex
+})
+
+const MainMobileConnected = connect(
+  mapStateToProps
+)(MainMobile)
+
+class Main extends Component {
+
+  componentDidMount () {
+    window.addEventListener('resize', this.setWidth)
+    this.setWidth()
+  }
+
+  setWidth = () => {
+    this.setState({ width: window.innerWidth })
+  }
+
+  render (props, { width }) {
+    return width >= 768 ? <MainDesktop /> : <MainMobileConnected />
+  }
+
+}
 
 export default Main
